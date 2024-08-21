7a769a2e206e4539d14921c79219911e228c5afd
modajb <83086103+modajb@users.noreply.github.com>
2024-08-21 21:37:37 +0330

    first commit

---
+++ Resiver/Program.cs
@@ -0,0 +1,65 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+//Create connection
+var connection = factory.CreateConnection();
+
+//Create channel
+var channel = connection.CreateModel();
+var queueName = "MyQueue01";
+channel.QueueDeclare(queueName, true, false, false, null);
+
+//create consumer
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, eventArg) =>
+{
+    var body = eventArg.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Resived message => {message}");
+    Thread.Sleep(1);
+    channel.BasicAck(eventArg.DeliveryTag, true );
+};
+
+channel.BasicConsume(queueName, false, consumer);
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("-----------------");
+Console.ReadKey();
+/*
+////Before 38
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+//Create connection
+var connection = factory.CreateConnection();
+
+//Create channel
+var channel = connection.CreateModel();
+var queueName = "MyQueue01";
+channel.QueueDeclare(queueName, false, false, false, null);
+
+//create consumer
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, eventArg) =>
+{
+    var body = eventArg.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Resived message => {message}");
+    Thread.Sleep(1000);
+};
+
+channel.BasicConsume(queueName, true, consumer);
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("-----------------");
+Console.ReadKey();
+
+*/
---
+++ Sender/Program.cs
@@ -0,0 +1,77 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//access to rabbitMQ
+//var factory = new ConnectionFactory();
+//factory.Uri = new Uri("amqp://guest:guest@localhost:15672");
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+//Create connection
+var connection = factory.CreateConnection();
+
+//Create channel
+var channel = connection.CreateModel();
+//crete Queue
+var queueName = "MyQueue01";
+//<param name="durable">Should this queue will survive a broker restart?</param>
+channel.QueueDeclare(queueName, true, false, false, null);
+int i = 1;
+var action = true;
+while (action)
+{
+    string message = $"this is a test meaasge{i++} from my sender at{DateTime.Now}";
+    var body = Encoding.UTF8.GetBytes(message);
+    //save messages in disc
+    var properties = channel.CreateBasicProperties();
+    properties.Persistent = true;
+
+    channel.BasicPublish("", queueName, properties, body);
+    if (i > 500)
+    {
+        action = false;
+    }
+}
+//for (int i = 0; i < 20; i++)
+//{
+//    string message = $"this is a test meaasge{i} from my sender at{DateTime.Now}";
+//    var body = Encoding.UTF8.GetBytes(message);
+//    //save messages in disc
+//    var properties = channel.CreateBasicProperties();
+//    properties.Persistent = true;
+
+//    channel.BasicPublish("", queueName, properties, body);
+//}
+channel.Close();
+connection.Close();
+
+
+/*
+////before 38
+//access to rabbitMQ
+//var factory = new ConnectionFactory();
+//factory.Uri = new Uri("amqp://guest:guest@localhost:15672");
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+//Create connection
+var connection = factory.CreateConnection();
+
+//Create channel
+var channel = connection.CreateModel();
+//crete Queue
+var queueName = "MyQueue01";
+//<param name="durable">Should this queue will survive a broker restart?</param>
+channel.QueueDeclare(queueName, true,false,false, null);
+for (int i = 0; i < 20; i++)
+{
+    string message = $"this is a test meaasge{i} from my sender at{DateTime.Now}";
+    var body = Encoding.UTF8.GetBytes(message);
+    //save messages in disc
+    var properties = channel.CreateBasicProperties();
+    properties.Persistent = true;
+
+    channel.BasicPublish("", queueName, properties, body);
+}
+channel.Close();
+connection.Close();
+ */
+
